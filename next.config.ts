5e2a6a1f0f91ac772bffe8eab4b68d0af6d88119
Brenda Awori <binomugishacom@gmail.com>
2026-07-14 22:09:51 +0300

    Add GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = 'joyfulhome';
+let assetPrefix = `/${repo}/`;
+let basePath = `/${repo}`;
+
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  assetPrefix: isGithubActions ? assetPrefix : undefined,
+  basePath: isGithubActions ? basePath : '',
 };
 
 export default nextConfig;
