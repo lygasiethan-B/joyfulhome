59af2eea1bdbcbbed3add434f182bac08ada496d
Brenda Awori <binomugishacom@gmail.com>
2026-07-14 23:49:59 +0300

    Shift mobile image even further to the right

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
           src={heroImg} 
           alt="Mother cradling her newborn at home" 
           fill
-          className="object-cover object-[65%_center] md:object-[105%_center]"
+          className="object-cover object-[45%_center] md:object-[105%_center]"
           priority
         />
         
